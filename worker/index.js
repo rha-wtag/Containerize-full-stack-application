52136cc77f641d8418e3416bf48edc5e06fddaec
Rakinul Haque <rakinul.haque@welldev.io>
2024-09-24 14:28:22 +0600

    nn

--- worker/index.js
+++ worker/index.js
@@ -2,7 +2,7 @@ const keys = require('./keys');
 const redis = require('redis');
 // const keys = require('./keys');
 // { redisHost, redisPort }
-const redisClient = createClient({
+const redisClient = redis.createClient({
     host: keys.redisHost,
     port: keys.redisPort,
     retry_strategy: () => 1000
